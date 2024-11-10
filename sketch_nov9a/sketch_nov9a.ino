8744ec3cd17cb824e99038166dce7ffe45603741
AmbivalentLeather <144703073+AmbivalentLeather@users.noreply.github.com>
2024-11-10 02:43:37 -0500

    Update sketch_nov9a.ino

--- sketch_nov9a/sketch_nov9a.ino
+++ sketch_nov9a/sketch_nov9a.ino
@@ -1,9 +1,9 @@
-int LEDpin[] = {13, 12, 11};
-int NEUpin[] = {7, 6, 5};
+int LEDpin[] = {13, 12, 11, 10, 9, 8};
+int NEUpin[] = {5, 4, 3, 2, 1, 0};
 
 int level;
 int delayT = 1;
-int delayL = 1000;
+int delayL = 0;
 
 int count;
 int permaCount = 100;
@@ -12,17 +12,27 @@ void setup() {
   pinMode(LEDpin[0], OUTPUT);
   pinMode(LEDpin[1], OUTPUT);
   pinMode(LEDpin[2], OUTPUT);
+  pinMode(LEDpin[3], OUTPUT);
+  pinMode(LEDpin[4], OUTPUT);
+  pinMode(LEDpin[5], OUTPUT);
 
   pinMode(NEUpin[0], OUTPUT);
   pinMode(NEUpin[1], OUTPUT);
   pinMode(NEUpin[2], OUTPUT);
+  pinMode(NEUpin[3], OUTPUT);
+  pinMode(NEUpin[4], OUTPUT);
+  pinMode(NEUpin[5], OUTPUT);
 
   digitalWrite(NEUpin[0], HIGH);
   digitalWrite(NEUpin[1], HIGH);
   digitalWrite(NEUpin[2], HIGH);
+  digitalWrite(NEUpin[3], HIGH);
+  digitalWrite(NEUpin[4], HIGH);
+  digitalWrite(NEUpin[5], HIGH);
 }
 void loop() {
   
+
   count = permaCount;
   while (count > 0) { 
     ledON(0, 0);
@@ -41,7 +51,6 @@ void loop() {
   delay(delayL);
   ledOFF(0, 1);
   ledOFF(1, 0);
-
 
   count = permaCount;
   while (count > 0) { 
@@ -73,6 +82,167 @@ void loop() {
   delay(delayL);
   ledOFF(2, 2);
 
+  count = permaCount;
+  while (count > 0) { 
+    ledON(2, 1);
+    ledON(1, 2);
+    count = count - 1;
+  }
+  delay(delayL);
+  ledOFF(2, 1);
+  ledOFF(1, 2);
+
+  count = permaCount;
+  while (count > 0) { 
+    ledON(2, 0);
+    ledON(1, 1);
+    ledON(0, 2);
+    count = count - 1;
+  }
+  delay(delayL);
+  ledOFF(2, 0);
+  ledOFF(1, 1);
+  ledOFF(0, 2);
+
+  count = permaCount;
+  while (count > 0) { 
+    ledON(0, 1);
+    ledON(1, 0);
+    count = count - 1;
+  }
+  delay(delayL);
+  ledOFF(0, 1);
+  ledOFF(1, 0);
+
+  count = permaCount;
+  while (count > 0) { 
+    ledON(0, 0);
+    count = count - 1;
+  }
+  delay(delayL);
+  ledOFF(0, 0);
+
+  count = permaCount;
+  while (count > 0) { 
+    ledON(0, 0);
+    count = count - 1;
+  }
+  delay(delayL);
+  ledOFF(0, 0);
+
+  count = permaCount;
+  while (count > 0) { 
+    ledON(3, 3);
+    count = count - 1;
+  }
+  delay(delayL);
+  ledOFF(3, 3);
+
+  count = permaCount;
+  while (count > 0) { 
+    ledON(3, 4);
+    ledON(4, 3);
+    count = count - 1;
+  }
+  delay(delayL);
+  ledOFF(3, 4);
+  ledOFF(4, 3);
+
+  count = permaCount;
+  while (count > 0) { 
+    ledON(5, 3);
+    ledON(4, 4);
+    ledON(3, 5);
+    count = count - 1;
+  }
+  delay(delayL);
+  ledOFF(5, 3);
+  ledOFF(4, 4);
+  ledOFF(3, 5);
+
+  count = permaCount;
+  while (count > 0) { 
+    ledON(5, 4);
+    ledON(4, 5);
+    count = count - 1;
+  }
+  delay(delayL);
+  ledOFF(5, 4);
+  ledOFF(4, 5);
+
+  count = permaCount;
+  while (count > 0) { 
+    ledON(5, 5);
+    count = count - 1;
+  }
+  delay(delayL);
+  ledOFF(5, 5);
+
+  count = permaCount;
+  while (count > 0) { 
+    ledON(5, 4);
+    ledON(4, 5);
+    count = count - 1;
+  }
+  delay(delayL);
+  ledOFF(5, 4);
+  ledOFF(4, 5);
+
+  count = permaCount;
+  while (count > 0) { 
+    ledON(5, 3);
+    ledON(4, 4);
+    ledON(3, 5);
+    count = count - 1;
+  }
+  delay(delayL);
+  ledOFF(5, 3);
+  ledOFF(4, 4);
+  ledOFF(3, 5);
+
+  count = permaCount;
+  while (count > 0) { 
+    ledON(3, 4);
+    ledON(4, 3);
+    count = count - 1;
+  }
+  delay(delayL);
+  ledOFF(3, 4);
+  ledOFF(4, 3);
+
+  count = permaCount;
+  while (count > 0) { 
+    ledON(3, 3);
+    count = count - 1;
+  }
+  delay(delayL);
+  ledOFF(3, 3);
+
+  // count = permaCount;
+  // while (count > 0) { 
+  //   ledON(1, 0);
+  //   ledON(2, 0);
+  //   ledON(0, 0);
+  //   ledON(0, 1);
+  //   ledON(0, 2);
+  //   ledON(1, 1);
+  //   ledON(1, 2);
+  //   ledON(2, 1);
+  //   ledON(2, 2);
+  //   count = count - 1;
+  // }
+  // delay(delayL);
+  // ledOFF(1, 0);
+  // ledOFF(2, 0);
+  // ledOFF(0, 0);
+  // ledOFF(0, 1);
+  // ledOFF(0, 2);
+  // ledOFF(1, 1);
+  // ledOFF(1, 2);
+  // ledOFF(2, 1);
+  // ledOFF(2, 2);
+  // delay(1000);
+
 }
 
 int ledON(int X, int Y) {
@@ -80,7 +250,6 @@ int ledON(int X, int Y) {
   digitalWrite(LEDpin[Y], HIGH);
   delay(delayT);
   digitalWrite(LEDpin[Y], LOW);
-  delay(delayT);
   digitalWrite(NEUpin[X], HIGH);
 }
 
